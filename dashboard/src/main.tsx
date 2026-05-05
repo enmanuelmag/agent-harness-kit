f3ff4317bd620950cbe22a5d3d178be63bb0cf30
enmanuelmag <enmanuelmag@cardor.dev>
2026-05-05 14:16:04 -0500

    refactor(main): reorganize import statements for clarity and consistency

--- dashboard/src/main.tsx
+++ dashboard/src/main.tsx
@@ -1,8 +1,10 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { createRouter,RouterProvider } from '@tanstack/react-router'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { RouterProvider, createRouter } from '@tanstack/react-router'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
 import { routeTree } from './routeTree.gen'
+
 import './styles/global.css'
 
 export const queryClient = new QueryClient({
